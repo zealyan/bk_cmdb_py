e96abeb5edd232a60cbd63243d39ebde8dcf3463
zealyan <zealyan@users.noreply.github.com>
2026-05-28 00:08:02 +0000

    feat: 拉取远端仓库代码
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- bk-cmdb-release-v3.10.41/src/ui/builder/config/index.js
+++ bk-cmdb-release-v3.10.41/src/ui/builder/config/index.js
@@ -77,34 +77,10 @@ const dev = {
   },
 
   proxyTable: {
-    '/proxy/user': {
-      logLevel: 'info',
-      changeOrigin: true,
-      target: 'http://192.168.45.141:8083/',
-      pathRewrite: {
-        '^/proxy/user': '/user'
-      }
-    },
-    '/proxy/logout': {
-      logLevel: 'info',
-      changeOrigin: true,
-      target: 'http://192.168.45.141:8083/',
-      pathRewrite: {
-        '^/proxy/logout': '/logout'
-      }
-    },
-    '/proxy/login': {
-      logLevel: 'info',
-      changeOrigin: true,
-      target: 'http://192.168.45.141:8083/',
-      pathRewrite: {
-        '^/proxy/login': '/login'
-      }
-    },
     '/proxy': {
       logLevel: 'info',
       changeOrigin: true,
-      target: 'http://192.168.45.141:8080/',
+      target: 'http://localhost:3000/',
       pathRewrite: {
         '^/proxy': ''
       }
@@ -112,7 +88,7 @@ const dev = {
   },
   // Various Dev Server settings
   host: 'localhost', // can be overwritten by process.env.HOST
-  port: 9090, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+  port: 9091, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
   autoOpenBrowser: true,
   errorOverlay: true,
   notifyOnErrors: true,
